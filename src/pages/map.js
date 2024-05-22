0839332c4c2cd7b467bb5f120635c04d2cec8a82
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2024-05-22 14:33:38 +0200

    FIX: display map

--- src/pages/map.js
+++ src/pages/map.js
@@ -6,46 +6,49 @@
 |  \/   \__,_|_| \__/\__|\__,_|\__,_|_|\___/  |_____|\___/_____|_/_/  |_____|\___/_____|  |_|   |
 |                                                                                               |
 \----------------------------------------------------------------------------------------------*/
-import { MapContainer, TileLayer, GeoJSON, LayersControl, MinimapControl } from 'react-leaflet';
+import useIsBrowser from '@docusaurus/useIsBrowser';
+
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
-import L from 'leaflet';
-import 'leaflet-defaulticon-compatibility';
+// import L from 'leaflet';
 
-import Markers from '@site/static/map/Markers.json';
-import Rivers from '@site/static/map/Rivers.json';
-import Routes from '@site/static/map/Routes.json';
-import Map from '@site/static/map/Map.json';
+export default function MapViewer() {
+    const isBrowser = useIsBrowser();
 
-const MapView = () => {
-    return (
-        <MapContainer center={[0, 0]} zoom={13} style={{ height: "100vh" }}>
-            <TileLayer
-                url="/map/tiles/{z}/{x}_{y}.png"
-                attribution='&copy; <a href="https://taistudio.fr/">Tai Studio</a>'
-            />
-            <LayersControl position="topright">
+    if(isBrowser){
+        const { MapContainer, TileLayer, GeoJSON, LayersControl, MinimapControl } = require('react-leaflet');
 
-                <LayersControl.Overlay name="Marker">
-                    <GeoJSON data={Markers} pathOptions={{ color: 'green' }} />
-                </LayersControl.Overlay>
+        const Markers = require('@site/static/map/Markers.json');
+        const Rivers = require('@site/static/map/Rivers.json');
+        const Routes = require('@site/static/map/Routes.json');
+        const Map = require('@site/static/map/Map.json');
 
-                <LayersControl.Overlay name="Rivers">
-                    <GeoJSON data={Rivers} />
-                </LayersControl.Overlay>
-
-                <LayersControl.Overlay name="Routes">
-                    <GeoJSON data={Routes} pathOptions={{ color: 'yellow' }} />
-                </LayersControl.Overlay>
-
-                <LayersControl.Overlay name="Map">
-                    <GeoJSON data={Map} pathOptions={{ color: 'purple' }} />
-                </LayersControl.Overlay>
-
-            </LayersControl>
-            {/* <MinimapControl position="topright" /> */}
-        </MapContainer>
-    );
-};
-
-export default MapView;
+        return (
+            <main>
+                 <MapContainer center={[0, 0]} zoom={13} style={{ height: "100vh" }}>
+                     <TileLayer
+                         url="/map/tiles/{z}/{x}_{y}.png"
+                         attribution='&copy; <a href="https://taistudio.fr/">Tai Studio</a>'
+                     />
+                     <LayersControl position="topright">
+                         <LayersControl.Overlay name="Marker">
+                             <GeoJSON data={Markers} pathOptions={{ color: 'green' }} />
+                         </LayersControl.Overlay>
+                         <LayersControl.Overlay name="Rivers">
+                             <GeoJSON data={Rivers} />
+                         </LayersControl.Overlay>
+                         <LayersControl.Overlay name="Routes">
+                             <GeoJSON data={Routes} pathOptions={{ color: 'yellow' }} />
+                         </LayersControl.Overlay>
+                         <LayersControl.Overlay name="Map">
+                             <GeoJSON data={Map} pathOptions={{ color: 'purple' }} />
+                         </LayersControl.Overlay>
+                     </LayersControl>
+                     {/* <MinimapControl position="topright" /> */}
+                 </MapContainer>
+            </main>
+        );
+    }
+    
+    return (<main></main>);
+}
